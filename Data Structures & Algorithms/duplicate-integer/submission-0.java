1035baf75aa023ddfcfc725bea3516adef9768c6
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-03-30 21:16:42 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer,Integer> h=new HashMap<>();
+        for(int i:nums){
+            if(h.containsKey(i)){
+                return true;
+            }
+            else{
+                h.put(i,0);
+            }
+        }
+        return false;
+    }
+}
